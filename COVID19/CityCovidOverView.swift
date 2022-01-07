2f11f5231770cb32a56fe8f7012f27be6acbce14
joensumin <soomis7@naver.com>
2022-01-07 15:25:55 +0900

    alamofire를 활용하여 서버로부터 데이터 호출

---
+++ COVID19/CityCovidOverView.swift
@@ -0,0 +1,41 @@
+//
+//  CityCovidOverView.swift
+//  COVID19
+//
+//  Created by Terry on 2022/01/07.
+//
+
+import Foundation
+
+struct CityCovidOverView: Codable {
+    let korea       :CovidOverView
+    let seoul       :CovidOverView
+    let busan       :CovidOverView
+    let daegu       :CovidOverView
+    let incheon     :CovidOverView
+    let gwangju     :CovidOverView
+    let daejeon     :CovidOverView
+    let ulsan       :CovidOverView
+    let sejong      :CovidOverView
+    let gyeonggi    :CovidOverView
+    let gangwon     :CovidOverView
+    let chungbuk    :CovidOverView
+    let chungnam    :CovidOverView
+    let jeonbuk     :CovidOverView
+    let jeonnam     :CovidOverView
+    let gyeongbuk   :CovidOverView
+    let gyeongnam   :CovidOverView
+    let jeju        :CovidOverView
+}
+
+struct CovidOverView: Codable {
+    let countryName :String
+    let newCase     :String
+    let totalCase   :String
+    let recovered   :String
+    let death       :String
+    let percentage  :String
+    let newCcase    :String
+    let newFcase    :String
+    
+}
--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -18,9 +20,41 @@ class ViewController: UIViewController {
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchCovidOverView(completionHandler: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result) :
+                debugPrint("success : \(result)")
+            case let .failure(error):
+                debugPrint("error : \(error )")
+            }
+        })
     }
 
+    //MARK: - function
+    func fetchCovidOverView(completionHandler: @escaping (Result<CityCovidOverView, Error>) -> Void){
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey" : "Hxvb51O8IgnKJiU4fRAcLmjoBswCh97da"
+        ]
+        
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data) :
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverView.self, from: data)
+                        completionHandler(.success(result))
+                    }catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
+        
+    }
 
 }
 
